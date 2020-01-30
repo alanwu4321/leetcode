c783d43c457aec67f0ed34832b525790b204cc10
Alan Wu <ac2wu@edu.uwaterloo.ca>
2020-01-30 02:19:45 -0500

    rotten oranges

--- basic.py
+++ basic.py
@@ -1,4 +1,5 @@
 import collections
+import sys
 
 """
 Node
@@ -6,6 +7,48 @@ Node
 class Node:
     def __init__(self):
         self.val = 1
+
+"""
+Sort
+"""
+a = [[10,20],[30,200],[400,50],[30,20]]
+diff = lambda val: abs(val[0]-val[1])
+sorted(a, key=diff, reverse= True)
+
+
+"""
+String
+"""
+
+
+"""
+Int
+"""
+
+sys.maxint
+-sys.maxint -1
+
+"""
+List
+"""
+
+print("\n======LIST=======\n")
+
+intialize = [0] * 100
+
+any(a)
+my_comp = [x * 5 for x in range(1000) if x%2==0]
+my_gen = (x * 5 for x in range(1000)if x%2==0)
+
+sys.getsizeof(my_comp) > sys.getsizeof(my_gen)
+
+test = [[1,2],[1],[3]]
+
+[1 in row for row in test ] #=> [true, true, true]
+any((1 in row for row in test)) #=> false
+
+
+
 """
 Loop
 """
@@ -42,6 +85,11 @@ t = ("a", 1, "b")
 
 print(t[1])
 
+# we can just destruct
+org = (1,2,3)
+a,b,c = org
+print(a)
+
 
 """
 Stack
@@ -53,11 +101,10 @@ stack.append(1)
 stack.pop()
 
 
-
 """
 Queue
 """
-
+print("\n======Queue=======\n")
 stack = list()
 
 stack.append(1)
@@ -65,4 +112,14 @@ stack.append(1)
 #pop from the front
 stack.pop(0)
 
+
+"""
+Class
+"""
+print("\n======CLASS=======\n")
+class Orange:
+    def __init__(self, row, col, layer):
+        self.row = row
+        self.col = col
+        self.layer = layer
 
